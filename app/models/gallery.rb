79069552d1b2d8fea32b5bff7d715cc3e1a1ec79
Hamish King <heymishy@gmail.com>
2012-03-07 20:09:56 +1100

    deleted:    app/controllers/photos_controller.rb 	deleted:    app/models/gallery.rb 	deleted:    app/models/photo.rb 	deleted:    app/views/gallery/upload.html.erb 	deleted:    app/views/photos/_form.html.erb 	deleted:    app/views/photos/edit.html.erb 	deleted:    app/views/photos/index.html.erb 	deleted:    app/views/photos/new.html.erb 	deleted:    app/views/photos/show.html.erb
    Removed extra views/controllers/models from playing around

--- app/models/gallery.rb
+++
@@ -1,6 +0,0 @@
-class Gallery
-  include Mongoid::Document
-  attr_accessible :name, :description
-  field :name, 		:type => String, :null => false, :default => ""
-  field :description, 	:type => String, :null => false, :default => ""
-end
--- app/models/photo.rb
+++
@@ -1,7 +0,0 @@
-require 'carrierwave/mongoid'
-
-class Photo
-  include Mongoid::Document
-  mount_uploader :image, ImageUploader
-#  validates_presence_of :name 
-end
